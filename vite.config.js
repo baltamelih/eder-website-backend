12a3c5240b32582e603831b22e663d143d5542c2
Melih Balta <85071478+baltamelih@users.noreply.github.com>
2025-12-23 03:41:58 +0300

    buffer added

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,35 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react({
       babel: {
-        plugins: [['babel-plugin-react-compiler']],
+        plugins: [["babel-plugin-react-compiler"]],
       },
     }),
   ],
+
+  // ✅ Buffer polyfill için gerekli
+  resolve: {
+    alias: {
+      buffer: "buffer",
+    },
+  },
+  optimizeDeps: {
+    include: ["buffer"],
+  },
+
   build: {
     rollupOptions: {
       output: {
         manualChunks: {
-          vendor: ['react', 'react-dom'],
-          router: ['react-router-dom'],
-          ui: ['antd', 'framer-motion']
-        }
-      }
-    }
-  }
-})
-
+          vendor: ["react", "react-dom"],
+          router: ["react-router-dom"],
+          ui: ["antd", "framer-motion"],
+        },
+      },
+    },
+  },
+});
