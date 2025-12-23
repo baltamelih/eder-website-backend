711b54c312f9d5352ca59bc555940279eba39179
Melih Balta <85071478+baltamelih@users.noreply.github.com>
2025-12-23 03:42:20 +0300

    main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,8 @@ import { router } from "./app/routes";
 import { theme } from "./app/theme";
 import { AuthProvider } from "./services/AuthContext";
 import { SubscriptionProvider } from "./services/SubscriptionContext";
-
+import { Buffer } from "buffer";
+window.Buffer = window.Buffer || Buffer;
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ConfigProvider theme={theme}>
